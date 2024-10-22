1d7b69c026ceb6c99ff0ba4d3e5ab786684baed5
cov <yusripawaki2@gmail.com>
2024-10-22 12:58:51 +0800

    "feat(documentation): add folder component "

---
+++ src/app/components/documentation/Folder.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+interface FolderProps {
+  name: string;
+  onDelete: () => void;
+  onClick: () => void;
+}
+
+const MySwal = withReactContent(Swal);
+
+const Folder: React.FC<FolderProps> = ({ name, onDelete, onClick }) => {
+  const handleDelete = () => {
+    MySwal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        onDelete();
+        MySwal.fire("Deleted!", "Your folder has been deleted.", "success");
+      }
+    });
+  };
+
+  return (
+    <div
+      className="relative bg-gray-500 p-4 rounded-lg shadow-md cursor-pointer"
+      onClick={onClick}
+    >
+      <h3 className="text-lg font-semibold">{name.toUpperCase()}</h3>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          handleDelete();
+        }}
+        className="absolute top-2 right-2 text-red-500"
+      >
+        ✖
+      </button>
+    </div>
+  );
+};
+
+export default Folder;
