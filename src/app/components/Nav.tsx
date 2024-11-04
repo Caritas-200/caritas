a22d0c06c25b60413ff1989c77ad2c02c21998f0
cov <yusripawaki2@gmail.com>
2024-11-04 17:38:22 +0800

    "style: improve nav"

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -4,27 +4,29 @@ import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx"; // For conditional class names
+import Image from "next/image";
 
 // Define the type for each navigation item
 interface NavItem {
   name: string;
   href: string;
+  icon: string;
 }
 
 // Navigation items array
 const navItems: NavItem[] = [
-  { name: "Home", href: "/home" },
-  { name: "List of Barangays", href: "/barangay" },
-  { name: "Documentation", href: "/documentation" },
-  { name: "Donors", href: "/donor" },
-  { name: "Profile", href: "/profile" },
+  { name: "Home", href: "/home", icon: "/icon/home.svg" },
+  { name: "List of Barangays", href: "/barangay", icon: "/icon/list.svg" },
+  { name: "Documentation", href: "/documentation", icon: "/icon/doc.svg" },
+  { name: "Donors", href: "/donor", icon: "/icon/donor.svg" },
+  { name: "Profile", href: "/profile", icon: "/icon/profile.svg" },
 ];
 
 const Nav: React.FC = () => {
   const pathname = usePathname(); // Get the current pathname
 
   return (
-    <nav className="bg-gray-800 text-white min-h-screen p-4 min-w-[200px]">
+    <nav className="bg-gray-800 text-white min-h-screen p-1 pt-2 pr-2 min-w-[220px]">
       <ul className="space-y-4">
         {navItems.map((item) => {
           const isActive = pathname === item.href;
@@ -34,13 +36,20 @@ const Nav: React.FC = () => {
               <Link href={item.href} passHref>
                 <button
                   className={clsx(
-                    "block px-4 py-2 rounded-lg transition-colors duration-200 whitespace-nowrap",
+                    "flex mt-4 gap-2 px-4 py-2 rounded-lg items-center w-full transition-colors duration-200 whitespace-nowrap",
                     {
                       "bg-gray-700": isActive,
                       "hover:bg-gray-600": !isActive,
                     }
                   )}
                 >
+                  <Image
+                    src={item.icon}
+                    alt=""
+                    width={30}
+                    height={30}
+                    className=""
+                  />
                   {item.name}
                 </button>
               </Link>
