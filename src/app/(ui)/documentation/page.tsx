55ebfe7b660d217ea915d0ccbff76815497ae7cc
cov <yusripawaki2@gmail.com>
2025-01-03 19:32:19 +0800

    "style(documentation): organize folder layout"

--- src/app/(ui)/documentation/page.tsx
+++ src/app/(ui)/documentation/page.tsx
@@ -70,17 +70,28 @@ const Documentation: React.FC = () => {
       folder.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const groupedFolders = filteredFolders.reduce((acc, folder) => {
+    const firstLetter = folder.name[0].toUpperCase();
+    if (!acc[firstLetter]) {
+      acc[firstLetter] = [];
+    }
+    acc[firstLetter].push(folder);
+    return acc;
+  }, {} as Record<string, typeof filteredFolders>);
+
+  const sortedKeys = Object.keys(groupedFolders).sort();
+
   return (
     <MainLayout>
       <Header />
       <div className="flex flex-row flex-1 bg-gray-700 text-gray-100">
         <LeftNav />
-        <div className="w-full overflow-y-auto p-4 h-svh pb-24">
+        <div className="w-full overflow-y-auto p-4 h-svh pb-24 ">
           <div className="p-4 w-full bg-gray-700 ">
             <h2 className="p-8 text-3xl font-bold mb-4 text-center text-white">
               Documentation
             </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4  border-b border-gray-500 mb-4">
               <div className="mb-4 flex gap-4 w-full">
                 <button
                   className="bg-blue-500 text-white py-2 px-4 rounded-lg whitespace-nowrap"
@@ -106,15 +117,22 @@ const Documentation: React.FC = () => {
                 />
               </div>
             </div>
-            <h1 className="mt-2">Folders:</h1>
-            <div className="grid mt-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-              {filteredFolders.map((folder) => (
-                <Folder
-                  key={folder.id}
-                  name={folder.name}
-                  onDelete={() => handleDeleteFolder(folder.name)}
-                  onClick={() => handleFolderClick(folder.name)}
-                />
+
+            <div className="flex flex-col gap-4 gap-y-4">
+              {sortedKeys.map((letter) => (
+                <div key={letter}>
+                  <h1 className="mb-4 text-xl">{letter}</h1>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-4">
+                    {groupedFolders[letter].map((folder) => (
+                      <Folder
+                        key={folder.id}
+                        name={folder.name}
+                        onDelete={() => handleDeleteFolder(folder.id)}
+                        onClick={() => handleFolderClick(folder.name)}
+                      />
+                    ))}
+                  </div>
+                </div>
               ))}
             </div>
           </div>
